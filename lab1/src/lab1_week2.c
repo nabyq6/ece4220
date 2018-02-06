96f107ffd7377c599dc355f2dd983c6b96a06593
nabyq6@mail.missouri.edu <nabyq6@mail.missouri.edu>
2018-02-05 18:46:48 -0600

    inserted commits into kernel code

--- lab1/src/lab1_week2.c
+++ lab1/src/lab1_week2.c
@@ -14,10 +14,13 @@ MODULE_LICENSE("GPL");
 
 int init_module(void)
 {
+	
 	//code to run when it is installed
+	//GPIO base addres is 0X3f200000
+	// interupt is 0x3f20001C
 	int *GPFSEL0 = ioremap(0x3f200000, 32);
 	int *GPSET0 = ioremap(0x3f20001C, 32); 
-	
+	//set bits 4,5,6 to 001, |= is OR function, with <<shifting left
 	*GPFSEL0 |= (001 << 6);
 	*GPSET0 |= ( 1 << 2);
 	*GPFSEL0 |= (001 << 9);
@@ -34,6 +37,7 @@ int init_module(void)
 void cleanup_module(void)
 {
 	int *GPCLR0 = ioremap( 0x3f200028, 32);
+	//clear 1st light at location 2
 	*GPCLR0 |= ( 1 << 2);
 	*GPCLR0 |= ( 1 << 3);
 	*GPCLR0 |= ( 1 << 4);		
