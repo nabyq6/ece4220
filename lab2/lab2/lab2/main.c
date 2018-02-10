8b15c9f02cb8de0e11aa09a76a4dc5267f317810
Nicholas Bouckaert <nabyq6@mail.missouri.edu>
2018-02-10 15:46:50 -0600

    try to run this on pi

--- lab2/lab2/lab2/main.c
+++ lab2/lab2/lab2/main.c
@@ -19,26 +19,41 @@ typedef struct info
     int row;
     int columns;
     int data[100][100];
+    int find;
+    int result;
+    
 }info;
 
 void import_info( char filename[], info *file);
+void *thread1(info *file);
 
 
 int main(void)
 {
-    //    pthread_t thread1, thread2, thread3;
+    pthread_t thread1; //thread2, thread3;
     struct info file1, file2, file3;
     info *fileI = &file1;
     info *fileII= &file2;
     info *fileIII = &file3;
+
+    float time, time_end, total_time;
+    
     
     import_info("2x100.txt", fileI);
     import_info("15x15.txt", fileII);
     import_info("20x10.txt", fileIII);
     
     
-    //array is bein import but over allocating space and it needs to be fixed before starting the search threading
+    //start the pthreading
     
+    //thread one will do the whole matrix
+    time = clock();
+    pthread_create( &thread1, NULL, thread1, (void *)&file1);
+    pthread_join( thread1, NULL);
+    time_end = clock();
+    printf("Single thread: ");
+    printf("rows in file: %d  columns in file %d \n", file1.row, file1.columns);
+    printf("Time to complete was: %fs\n", time - time_end); 
     
     
     
@@ -49,13 +64,33 @@ int main(void)
     
 }
 
+void *thread1(info *file)
+{
+    int i, j;
+    
+    for( i = 0; i < file->row; i++ )
+    {
+        for( j = 0; j < file->columns; j++)
+        {
+            if(file->data[i][j] == file->find)
+                {
+                    ++file->result;
+                }
+        }
+            
+    }
+    return 0;
+}
+
+
+
 void import_info( char filename[],  info *file)//function will open and read in every file and store in the stuct
 {
     int j, i;
     FILE *fp = fopen( filename, "r");
     fscanf(fp,"%d", &file->row);
     fscanf(fp,"%d", &file->columns);
-    printf("rows:%d , columns: %d", file->row, file->columns);
+    //printf("rows:%d , columns: %d", file->row, file->columns);
     
     //int array[file->row][file->columns];
     //int array= *(int*)malloc(sizeof(int)*file.columns*file.row);
@@ -65,7 +100,7 @@ void import_info( char filename[],  info *file)//function will open and read in 
         for( j = 0; j < file->columns; j++)
         {
             fscanf(fp, "%d", &file->data[i][j]);
-            printf("  %d  ", file->data[i][j]);
+            //printf("  %d  ", file->data[i][j]);
         }
         
     }
@@ -73,4 +108,3 @@ void import_info( char filename[],  info *file)//function will open and read in 
     //printf(" %d \n", file->data[0][5]);
     fclose(fp);
 };
-
