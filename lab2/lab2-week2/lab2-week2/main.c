88ff15ece418cfe487a331d5f9ebf0b24cfed88c
Nicholas Bouckaert <nabyq6@mail.missouri.edu>
2018-02-13 10:14:15 -0600

    Submiting xcode project file

---
+++ lab2/lab2-week2/lab2-week2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  lab2-week2
+//
+//  Created by Nicholas Bouckaert on 2/13/18.
+//  Copyright © 2018 Nicholas Bouckaert. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
