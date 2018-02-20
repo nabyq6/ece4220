186d3665b98ee8af4129e6a956a11782fa97aacf
Nicholas Bouckaert <nabyq6@mail.missouri.edu>
2018-02-20 12:52:48 -0600

    commit beginning of code for lab3 _week1 work with the kernel load to make work

--- lab3/lab3/lab3/main.c
+++ lab3/lab3/lab3/main.c
@@ -7,9 +7,63 @@
 //
 
 #include <stdio.h>
+#include <unistd.h>
+#include <sched.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+ // libraries only on the pie
+/*
+#include <wiringPi.h>
+#include <sys/timer.h>
+#include "ece4220lab3.c"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+*/
+
+#define MY_PRIORITY 51
+void set_thread_priority( int change_priority );
+
+int main ( void )
+{
+    // WILL NEED SUDO TO RUN WITH PRIORITY CHANGE
+    set_thread_priority(0);
+    
+    //Setting up the wiring pi
+    wiringPiSetupGpio();
+    pinmode( 2, OUTPUT);
+    pinmode( 2, OUTPUT);
+    pinmode( 4, OUTPUT);
+    pinmode( 5, OUTPUT);
+    pinmode( 16, INPUT);
+    pullUpDnControl( 16, PUD_DOWN); // sett button press to zero
+    
+    digitalWrite(2,0);// set lights to off
+    digitalWrite(3,0);
+    digitalWrite(4,0);
+    digitalWrite(5,0);
+    
+    
+    
+    
+    
+    
+    
     return 0;
+}
+
+void set_thread_priority( int change_priority)// will be helpfull when setting the  priority in lab 3 part 2 with # threads
+{
+    struct sched_param param;
+    
+        param.sched_priority = MY_PRIORITY + change_priority;
+        if( sched_setscheduler(0, SCHED_FIFO, &param) < 0 )
+        {
+            printf("Real time is not accessed\n");
+            exit(1);
+        }
+    
+    
+            
 }
