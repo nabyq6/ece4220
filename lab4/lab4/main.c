c9bba50d99b2dd5a688c25e2f94e9d4c51d6485f
Nicholas Bouckaert <nabyq6@mail.missouri.edu>
2018-03-06 10:29:49 -0600

    first stuff for set up

--- lab4/lab4/main.c
+++ lab4/lab4/main.c
@@ -7,9 +7,38 @@
 //
 
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sched.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <time.h>
+#include <pthread.h>
+/* included and works only on the pi
+#include <wiringPi.h>
+#include <semaphore.h>
+#include <sys/timerfd.h>
+#include "serial_ece4220.h"
+ */
+
+#define MY_PRIORITY 51
+
+void set_thread_priority( int change_priority);
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
     return 0;
+}
+
+void set_thread_priority( int change_priority)//set priority for every
+{
+    struct sched_param param;
+    param.sched_priority = MY_PRIORITY + change_priority;
+    if( sched_setscheduler( 0, SCHED_FIFO, &param) < 0)
+    {
+        printf("Real Time is not accessed\n");
+        exit(1);
+    }
+    
 }
