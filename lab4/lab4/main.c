f3bff38aa457982b84c3f638d7310b76ba772ccd
Nicholas Bouckaert <nabyq6@mail.missouri.edu>
2018-03-09 12:34:55 -0600

    probably an issue with reading in the button press through the pipe '

--- lab4/lab4/main.c
+++ lab4/lab4/main.c
@@ -32,21 +32,28 @@ struct gps_data_buffer{
 
 
 void set_thread_priority( int change_priority);
+void *button_push_collect_time( void * input );
+
+//define GPIO ports
+#define Button1 27
 
 int main(int argc, const char * argv[]) {
     int pipe_to_gps; //for pipe from GPS exe.
     unsigned char location_data;
     struct timeval data_time;
    // Realtime();
-   
+    pthread_t check_button_press;
+    int time_collection_pipe;
     
     if((pipe_to_gps = open("/tmp/N_pipe1", O_RDONLY))< 0)
         {
-            printf("Connection to GPS was not made - N_pipe1 error");
+            printf("Connection to GPS was not made - N_pipe1 error\n");
             exit(-1);
         }
     
     printf("Connection to GPS device was successful");//only if both progarms are running at
+    
+    pthread_create(&check_button_press, NULL, &button_push_collect_time, NULL);
     
     while(1)
     {
@@ -62,11 +69,72 @@ int main(int argc, const char * argv[]) {
         gps_data_buffer.gps_data = location_data;
         gps_data_buffer.location_time = data_time;
     }
-    
+   
+    pthread_join( check_button_press, NULL);
+   
+    return 0;
     
 }
 
-/*
+void *button_push_collect_time( void * input )
+{
+    set_thread_priority(5);//hard cording thread prioirty to be 55
+    struct timeval Button_time;
+    int time_collection_pipe;
+    
+    int timer_fd = timer_fdcreate(CLOCK_MONOTONIC, 0);
+    
+    struct itimerspec itval;// setting timer to run thread every 75ms
+        itval.it_interval.tv_sec = 1; //Period_seconds
+        itval.it_interval.tv_nsec = 0;//peroid in nano
+        itval.it_value.tv_sec = 0;// init time in seconds
+        itval.it_value.tv_nsec = 75000000;// init time in nano
+    
+    if( -1 == timerfd_settime( timer_fd, 0, &itval, NULL)
+        {
+            printf("timer failed to create for the button push\n");
+            exit(-1);
+        }
+       //setting up to read from the first button
+       wiringPiSetup();
+       pinMode( Button1, INPUT);
+       pullUpDnControl( Button1, PUD_DOWN);
+       
+       uint64_t num_peroids = 0;
+       read( timer_fd, &num_peroids, sizeof( num_peroids));
+       while(1)
+    {
+       if(check_button() == 1)
+       {
+           clock_gettimeofday(&Button_time, NULL);
+           printf("Button Pressed\n");
+           fflush(stdout);
+           if(write(time_collection_pipe, &Button_time, sizeof(Button_time)) < 0)
+           {
+               printf("Error writing time to pipe in button_push_collect_time thread \n");
+           }
+           clear_button();
+        }
+        uint64_t num_periods = 0;                    //Wait one period
+        read(timer_fd, &num_periods, sizeof(num_periods));
+    }
+       pthread_exit(NULL);
+}
+       
+void *collect_button_information( void *input )
+    {
+        int collect_button_information= *((int*)input);
+        set_thread_priority(10);
+        struct timespec collected;
+        printf("Reading from the button press");
+        if( read(collect_button_information, &collected, sizeof(collect_button_information)))
+        {
+            printf("Error reading the information from the button press");
+        }
+        printf("reading collected_button_information occured");
+        
+        pthread_exit(NULL);
+    }
  void set_thread_priority( int change_priority)//set priority for every thread - reused from lab 3
 {
     struct sched_param param;
@@ -78,4 +146,6 @@ int main(int argc, const char * argv[]) {
     }
     
 }
- */
+       
+
+
