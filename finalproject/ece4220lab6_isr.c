86dd4ac0d3a649aed3d585cf9ea06d051ec0d44b
Nicholas Bouckaert <nabyq6@mail.missouri.edu>
2018-04-25 19:53:48 -0500

    rename and edit these files for the final project should cut down on the coding

---
+++ finalproject/ece4220lab6_isr.c
@@ -0,0 +1,289 @@
+/* ece4220lab6_isr.c
+ * ECE4220/7220
+ * Author: Luis Alberto Rivera
+ 
+ Basic steps needed to configure GPIO interrupt and attach a handler.
+ Check chapter 6 of the BCM2835 ARM Peripherals manual for details about
+ the GPIO registers, how to configure, set, clear, etc.
+ 
+ Note: this code is not functional. It shows some of the actual code that you need,
+ but some parts are only descriptions, hints or recommendations to help you
+ implement your module.
+ 
+ You can compile your module using the same Makefile provided. Just add
+ obj-m += YourModuleName.o
+ */
+
+#ifndef MODULE
+#define MODULE
+#endif
+#ifndef __KERNEL__
+#define __KERNEL__
+#endif
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <asm/io.h>
+#include <linux/init.h>
+#include <linux/types.h>
+#include <linux/interrupt.h>
+#include <linux/delay.h>
+#include <linux/fs.h>
+#include <asm/uaccess.h>
+#include <linux/kthread.h>
+#include <linux/sched.h>
+#include <linux/time.h>
+
+MODULE_LICENSE("GPL");
+
+/* Declare your pointers for mapping the necessary GPIO registers.
+   You need to map:
+   
+   - Pin Event detect status register(s)
+   - Rising Edge detect register(s) (either synchronous or asynchronous should work)
+   - Function selection register(s)
+   - Pin Pull-up/pull-down configuration registers
+   
+   Important: remember that the GPIO base register address is 0x3F200000, not the
+   one shown in the BCM2835 ARM Peripherals manual.
+*/
+#define MSG_SIZE 40
+#define CDEV_NAME "lab6"
+
+static unsigned long *BPtr, *GPSEL0, *GPSEL1, *GPSEL2, *GPSET0, *GPCLR0, *GPEDS0, *GPREN0, *GPPUD, 
+ *GPPUDCLK;
+
+static struct task_struct *kthread1;
+int major;
+int mydev_id;	// variable needed to identify the handler
+int n = 100;
+char buffer [MSG_SIZE];
+char try [MSG_SIZE];
+
+int my_kthread(void *ptr)//taken from the kthread part one almost exactly
+{
+	while(1)
+	{
+		*GPSET0 = *GPSET0 | 0x00000040;
+		udelay(n);
+		*GPCLR0 = *GPCLR0 | 0x00000040;
+		udelay(n);
+	
+		if(kthread_should_stop())
+		{
+			printk("thread was exited\n");
+			do_exit(0);
+		}
+	}
+	return 0;
+}
+// Interrupt handler function. Tha name "button_isr" can be different.
+// You may use printk statements for debugging purposes. You may want to remove
+// them in your final code.
+
+static ssize_t device_read( struct file *filp, char __user *buff, ssize_t lenght, loff_t *offset)
+{	
+//	printk("buffer in read:  %s", try);
+	ssize_t dummy = copy_to_user(buff, try, lenght);
+	
+	try[0] = '\0';
+
+	return lenght;
+
+}
+static ssize_t device_write( struct file *filp, char __user *buff, size_t len, loff_t *off)
+{
+	ssize_t  dummy;
+	
+	if ( len > MSG_SIZE)
+	{
+	return -EINVAL;
+	}
+
+	dummy = copy_from_user(buffer, buff, len);
+	
+	if( len == MSG_SIZE)
+	{
+	buffer[len - 1] = '\0';
+	}
+	else
+	{
+	buffer[len] = '\0';
+	}
+	
+	switch (buffer[1])
+	{
+	case 'A':
+	//	printk("button 1");
+		strcpy(buffer,"@A");
+		n = 300;
+		break;
+
+	case 'B':
+	//	printk("button 2");
+		strcpy(buffer,"@B");
+
+		n = 600;
+		break;
+	case 'C':
+	//	printk("button 3");
+		n = 900;
+		break;
+	case 'D':
+	//	printk("button 4");
+		n = 1200;
+		break;
+	case 'E':
+	//	printk("button 5");
+		n = 1400;
+		break;
+	}
+	
+	return len;
+
+
+}
+
+static struct  file_operations fops = {
+	.write = device_write,
+	.read = device_read,
+};
+
+static irqreturn_t button_isr(int irq, void *dev_id)
+{
+	// In general, you want to disable the interrupt while handling it.
+	disable_irq_nosync(79);
+
+	// This same handler will be called regardless of what button was pushed,
+	// assuming that they were properly configured.
+	// How can you determine which button was the one actually pushed?
+		
+	// DO STUFF (whatever you need to do, based on the button that was pushed)
+
+	// IMPORTANT: Clear the Event Detect status register before leaving.
+	switch( *GPEDS0)
+	{
+		case 0x10000:
+			n = 300;
+			strcpy(try,"@A");
+			printk("buffer is: %s\n", buffer);
+			break;
+		case 0x20000:
+			n = 600;
+			strcpy(try,"@B");
+			printk("buffer is: %s\n", buffer);
+			break;
+		case 0x40000:
+			n = 900;
+			strcpy(try,"@C");
+			printk("buffer is: %s\n", buffer);
+			break;
+		case 0x80000:
+			n = 1200;
+			strcpy(try,"@D");
+			printk("buffer is: %s\n", buffer);
+			break;
+		case 0x100000:
+			n = 1400;
+			strcpy(try,"@E");
+			printk("buffer is: %s\n", buffer);
+			break;
+	}
+	
+	*GPEDS0 = *GPEDS0 | 0x001F0000;//clear
+//	printk("Interrupt handled\n");	
+	enable_irq(79);		// re-enable interrupt
+	
+    return IRQ_HANDLED;
+}
+
+int init_module()
+{
+	int dummy = 0;
+	char kthread[] = "mythread";
+
+	BPtr = (unsigned long *) ioremap( 0x3f200000, 4096);
+
+	// Map GPIO registers
+	// Remember to map the base address (beginning of a memory page)
+	// Then you can offset to the addresses of the other registers
+
+	// Don't forget to configure all ports connected to the push buttons as inputs.
+	
+	// You need to configure the pull-downs for all those ports. There is
+	// a specific sequence that needs to be followed to configure those pull-downs.
+	// The sequence is described on page 101 of the BCM2835-ARM-Peripherals manual.
+	// You can use  udelay(100);  for those 150 cycles mentioned in the manual.
+	// It's not exactly 150 cycles, but it gives the necessary delay.
+	// WiringPi makes it a lot simpler in user space, but it's good to know
+	// how to do this "by hand".
+		
+	// Enable (Async) Rising Edge detection for all 5 GPIO ports.
+			
+	// Request the interrupt / attach handler (line 79, doesn't match the manual...)
+	// The third argument string can be different (you give the name)
+	
+	GPSEL0 = BPtr;
+	GPSET0 = BPtr +7;
+	GPCLR0 = BPtr + 10;
+	GPSEL1 = BPtr + 1;
+	GPSEL2 = BPtr + 2;
+	GPEDS0 = BPtr +   16;
+	GPREN0 = BPtr + 19;
+	GPPUD = BPtr + 37;
+	GPPUDCLK = BPtr + 38;
+	
+	*GPSEL0 = *GPSEL0 | 0x00040000;
+	*GPSEL1 = *GPSEL1 & 0xc003ffff;
+	*GPSEL2 = *GPSEL2 & 0xfffffff8;
+	
+	*GPREN0 = *GPREN0 | 0x001f0000;
+	
+	*GPPUD = *GPPUD | 0x00000001;
+	
+	*GPPUDCLK = *GPPUDCLK | 0x001f0000;
+	udelay(10);
+	
+	*GPPUD = *GPPUD & 0xfffffffe;
+	*GPPUDCLK  = *GPPUDCLK & 0xffe0ffff;
+	
+	kthread1 = kthread_create( my_kthread, NULL, kthread);
+	if(kthread1)
+	{
+		wake_up_process(kthread1);
+	}
+
+	major = register_chrdev( 0, CDEV_NAME, &fops);
+	printk("major is: %d", major);
+	if(major < 0)
+	{
+	printk("Registering the character device failed with %d\n", major);
+	return major;
+	}
+	printk("create char device (node) with suko mknod /dev/%s c %d 0\n", CDEV_NAME, major);
+	
+	dummy = request_irq(79, button_isr, IRQF_SHARED, "Button_handler", &mydev_id);
+
+	printk("Button Detection enabled.\n");
+	return 0;
+}
+
+// Cleanup - undo whatever init_module did
+void cleanup_module()
+{
+	// Good idea to clear the Event Detect status register here, just in case.
+		
+	// Disable (Async) Rising Edge detection for all 5 GPIO ports.
+	
+	// Remove the interrupt handler; you need to provide the same identifier
+	kthread_stop( kthread1 );
+	*GPEDS0 = *GPEDS0 | 0x001f0000;
+	*GPREN0 = *GPREN0 | 0xffe0ffff;
+	
+	unregister_chrdev(major, CDEV_NAME);
+
+	printk("CHar Device /dev/%s ungregistered\n", CDEV_NAME);
+         free_irq(79, &mydev_id);
+	
+	printk("Button Detection disabled.\n");
+}
---
+++ finalproject/part3.c
@@ -0,0 +1,317 @@
+//Nicholas Bouckaert
+//Lab 5
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <ifaddrs.h>
+#include <time.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define MSG_SIZE 40
+#define CHAR_DEV "/dev/lab6"
+void *readthread(void *check_this_bitch);
+
+// alot of code if not most of it is copied and used from lab5 sockets
+int sock;
+int cdev_id;
+struct sockaddr_in addr, from;
+int master = 0;
+int information_error_check, number ;
+
+char *findIP(void){//taken exactly from lab 5
+    int fd;                                                 //IP finding code sourced and modified from
+    struct ifreq ifr;
+    
+    fd = socket(AF_INET, SOCK_DGRAM, 0);
+    
+    /* I want to get an IPv4 IP address */
+    ifr.ifr_addr.sa_family = AF_INET;
+    
+    /* I want IP address attached to "eth0" */
+    strncpy(ifr.ifr_name, "wlan0", IFNAMSIZ-1);
+    
+    ioctl(fd, SIOCGIFADDR, &ifr);
+    
+    close(fd);
+    return inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr);
+}
+
+int main ( int argc, char *argv[])
+{
+//int information_error_check, number ;
+int master = 0;
+unsigned int length;
+socklen_t fromlen;
+struct sockaddr_in server;
+//struct sockaddr_in addr, from;
+char buffer[MSG_SIZE];    // to store received messages or messages to be sent.
+char my_message[MSG_SIZE];
+char vote_string[MSG_SIZE];
+char random_vote[2];
+int vote_number;
+int boolval = 1;            // for a socket option
+int note;
+char notes[MSG_SIZE];
+int flag = 0;
+//char bonus[MSG_SIZE];
+int dummy; 
+//int cdev_id;
+    
+    printf("start of progarm\n");
+    if (argc != 2)
+        {
+            printf("usage: %s port\n", argv[0]);
+            exit(-1);
+        }
+   printf("Argv\n"); 
+    sock = socket(AF_INET, SOCK_DGRAM, 0); // Creates socket. Connectionless.
+    if (sock < 0)
+        {
+            error("socket");
+        }
+    
+    //printf("socked made\n");
+    length = sizeof(server); // determines lenght of the structure
+    bzero(&server, length); // set all valus = 0
+    server.sin_family = AF_INET; //constant for internet domain
+    server.sin_addr.s_addr= INADDR_ANY; // MY IP address
+    server.sin_port = htons(atoi(argv[1]));
+    // going to set broadcasting address
+    
+    if (bind(sock, (struct sockaddr *)&server, length) < 0)
+        {
+            printf("binding error dumby\n");
+        }
+    printf("socked bind\n");
+    if (setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &boolval, sizeof(boolval)) < 0)
+        {
+            printf("error setting socket option\n");
+            exit(-1);
+        }
+    printf("setsockopt\n");
+    if((cdev_id = open( CHAR_DEV, O_RDWR)) == -1)
+    {
+        printf("cant connect to device properly %s\n", CHAR_DEV);
+	exit(-1);
+    }
+    fflush(stdout);
+    printf("after the open\n"); 
+    fromlen = sizeof(struct sockaddr_in);
+    strcpy( my_message, "Nicholas");
+    strcat( my_message, findIP());
+    strcat( my_message, "is your master");
+    printf("creating thread for bonus\n\n");
+	pthread_t read;
+	pthread_create(&read, NULL, &readthread, (void *)&master);
+ //   printf("in the while loop\n");
+    while(1)
+    {
+	//printf("seg fault\n");
+        bzero(buffer, MSG_SIZE);
+        information_error_check = recvfrom(sock, buffer, MSG_SIZE, 0, (struct sockaddr *)&addr, &fromlen);
+        if( information_error_check < 0)
+        {
+            error("that was an error receiving infromation from the sock connection\n");
+            exit( -1);
+        }
+        printf("Received where am i was:\n %s", buffer);
+        
+        
+        if(strcmp(buffer, "WHOIS\n") == 0)
+        {
+            printf("Received WHOIS\n");
+            
+            if(master == 1)
+            {
+                printf("Master SENDTO\n");
+                information_error_check = sendto(sock, my_message, MSG_SIZE, 0, ( struct sockaddr*)&addr, fromlen);
+                if( information_error_check < 0 )
+                {
+                    printf("error sending the imformation to server\n");
+                    exit(-1);
+                }
+                
+            }
+	}
+
+//	printf("seg fault check number 2");
+      if(strcmp(buffer, "VOTE\n") == 0)
+	{
+	printf("sent in vote\n");
+	master = 1;
+	number = rand() % 10 +1;
+	snprintf(random_vote, 10, "%d", number);
+	vote_string[0] = '\0';
+	strcpy(vote_string, "# ");
+	strcat(vote_string, findIP());
+	strcat(vote_string, " ");
+	strcat(vote_string, random_vote);
+	
+        addr.sin_addr.s_addr = inet_addr("128.206.19.255");
+        printf("in vote: boardcast id set\n");// error check
+	
+        information_error_check = sendto( sock, vote_string, sizeof(vote_string), 0, (struct sockaddr *) &addr, sizeof(addr));
+        if( information_error_check < 0)
+        {
+            printf("Error sending the information to server\n");
+        }
+        printf("Vote Sent: %s\n", vote_string);
+
+	}
+
+    if(buffer[0] == '#')
+    {
+        switch(buffer[16])
+        {
+            case  '1':
+                if(buffer[17] == '0'){
+                    vote_number = 10;
+                }else{
+                    vote_number = 1;
+                }
+                break;
+            case '2':
+                vote_number = 2;
+                break;
+            case '3':
+                vote_number = 3;
+                break;
+            case '4':
+                vote_number = 4;
+                break;
+            case '5':
+                vote_number = 5;
+                break;
+            case '6':
+                vote_number = 6;
+                break;
+            case '7':
+                vote_number= 7;
+                break;
+            case '8':
+                vote_number = 8;
+                break;
+            case '9':
+                vote_number = 9;
+                break;
+        }
+        if( number < vote_number)
+        {
+            master = 0;
+        }
+        else if(number == vote_number)
+        {
+            if(strncmp(vote_string,buffer,14) < 0)
+            {
+                master = 0;
+            }
+            else
+            {
+                master = 1;
+            }
+        }
+    }
+
+
+        
+        if( buffer[0] == '@')
+           {
+               switch(buffer[1])
+               {
+                   case 'A':
+                       note = 1;
+                       break;
+                
+                   case 'B':
+                       note = 2;
+                       break;
+                       
+                   case 'C':
+                       note = 3;
+                       break;
+                       
+                   case 'D':
+                       note = 4;
+                       break;
+                       
+                   case 'E':
+                       note = 5;
+                   break;
+                }
+               if(master == 1 && buffer [0] == '@' && flag == 0 )
+               {
+               addr.sin_addr.s_addr = inet_addr("128.206.19.255");
+               //printf("boardcast id set\n");// error check
+	//	buffer[0] = '\0';
+		strcpy( notes, buffer);
+		//strcat(notes, buffer[1]);
+		usleep(100);
+               information_error_check = sendto( sock, notes , sizeof(notes), 0, (struct sockaddr *) &addr, sizeof(addr));
+               if( information_error_check < 0)
+               {
+                   printf("Error sending the information to server\n");
+               }
+		flag = 1;
+               if(write(cdev_id , buffer, sizeof(buffer)) != sizeof(buffer))
+               {
+                   printf("error during note changing\n");
+                   exit(-1);
+               }
+	
+           }
+	else 
+	{
+	flag = 0;
+	}
+        
+    }
+}
+           return 0;
+	pthred_join( read, NULL);
+}
+
+void *readthread( void *check_that_bitch)
+{
+	int dummy;	
+	
+	char bonus[MSG_SIZE];
+	while(1)
+	{
+	bzero(bonus, MSG_SIZE);
+	//dummy = read(cdev_id, bonus, sizeof(bonus));	
+	if( read(cdev_id, bonus, sizeof(bonus)) == -1)
+	{
+	printf("error with the read");
+	}
+//	printf("%s\n", bonus);
+	if( bonus[0] == '@')
+	{
+//	printf("master is: %d  bonus is: %s\n", master, bonus);
+
+	if( *(int*)check_that_bitch == 1)
+	{
+//	printf("master is: %d  bonus is: %s\n", master, bonus);
+
+	information_error_check = sendto( sock, bonus, sizeof(bonus), 0, (struct sockaddr *) &addr, sizeof(addr));
+               if( information_error_check < 0)
+              	 {
+                	   printf("Error sending the information to server\n");
+             	  }
+
+	}
+    }
+	usleep(800);
+	bonus[0] = '\0';
+	}
+}
