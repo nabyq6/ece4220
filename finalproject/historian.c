d91c63afe6f17bf17c557fda5790799d54584a7d
Bouckaert <nabyq6@mail.missouri.edu>
2018-05-02 00:24:23 -0500

    historian working

--- finalproject/historian.c
+++ finalproject/historian.c
@@ -6,6 +6,11 @@
 //  Copyright © 2018 Nicholas Bouckaert. All rights reserved.
 //
 
+//compile using gcc historian.c -lpthread -lsqlite3
+
+//#include <iostream>
+//#include <fstream>
+//#include <cstring>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -20,12 +25,17 @@
 #include <net/if.h>
 #include <sys/ioctl.h>
 #include <sqlite3.h>
+#include <pthread.h>
+//#include <vector>
+//#include <string> 
+#include <unistd.h>
+//#include <iomanip>
 
-#define MSG_SIZE 100;
-#define port 4000;
-#define pi_address "128.206.19.17"; //dont forget to change
-#define historian_address "128.206.19.255"; //dont forget to change 
-#define database location "/desktop/4222.db";// location of the database on the local machine
+#define MSG_SIZE 100
+#define port 4000
+#define pi_address "128.206.19.255" //dont forget to change
+#define historian_address "128.206.19.255" //dont forget to change 
+#define database_location "/desktop/database.db"// location of the database on the local machine
 
 //fuctions below main
 void send_to_database_log( char message);
@@ -39,11 +49,11 @@ void database_connection();
 int sock, information_error_check, number ;
 unsigned int length;
 socklen_t fromlen;
-struct sockaddr_in server, send, receive;
+struct sockaddr_in server, send_this, receive;
 struct sockaddr_in addr, from;
 char buffer[MSG_SIZE];
 
-sqlite *dbfile;
+sqlite3 *dbfile;
 
 char *findIP(void){
     int fd;                                                 //IP finding code sourced and modified from
@@ -73,8 +83,10 @@ int main( int argc, char *argv[])
          
     sock = socket(AF_INET, SOCK_DGRAM, 0); // Creates socket. Connectionless.
     if (sock < 0)
-        error("socket");
-    
+	{
+        printf("socket error\n");
+    	}
+	
     // change socket permissions to allow broadcast
     if (setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &boolval, sizeof(boolval)) < 0)
     {
@@ -99,18 +111,18 @@ int main( int argc, char *argv[])
 	//reading and writing from the sockets 
 	pthread_t read_sock, write_sock;
 	
-	pthread_create(&read_sock, NULL, receive_update, NULL);
-	pthread_create(&write_sock, NULL, send_command, NULL); 
+	pthread_create(&read_sock, NULL, &receive_update, NULL);
+	pthread_create(&write_sock, NULL, &send_command, NULL); 
 
 	//start accessing the database
-	database_connection();
+//	database_connection();
 	
 	while(1)
 	{
 	usleep(2000);
 	}
-	pthread_join( receive_update, NULL);
-	pthread_join( send_command, NULL);
+	pthread_join( read_sock, NULL);
+	pthread_join( write_sock, NULL);
 	
 return 0;
 }
@@ -122,12 +134,12 @@ void database_connection()
 	
 	if( rc )
 	{
-		fprintf(stderr, "Could not open the database:%s\n" sqlite3_errmsg(dbfile));
+		fprintf(stderr, "Could not open the database:%s\n", sqlite3_errmsg(dbfile));
 		exit(-1);
 	} 
 	else
 	{
-	 fprintf("opened the database successfully\n");
+	printf("opened the database successfully\n");
 	}
 
 	
@@ -136,13 +148,15 @@ void send_to_database_log( char message)
 {	//database code used was from CMP3830
 	sqlite3_stmt *statement;
 	char *err_msg = 0;
-	string insert_this;
+	char insert_this;
 	
-	string construct = "INSERT INTO Log VALUES";
+	char cons[200] = "INSERT INTO Log VALUES";
 	
-	insert_this = construct + to_string(messge);
-	
-	char *str = &insert_this[0];
+	//insert_this = cons + to_string(messge);
+	strcat( cons, &message);
+	printf("%s\n", cons);
+
+	char *str = cons;
 	char *query = str;
 	int res = 0;
 
@@ -165,13 +179,15 @@ void send_to_database_error( char message)
 {
 	sqlite3_stmt *statement;
 	char *err_msg = 0;
-	string insert_this;
+	char  insert_this;
 	
-	string construct = "INSERT INTO error VALUES";
+	char construct[200] = "INSERT INTO error VALUES";
 	
-	insert_this = construct + to_string(messge);
+	//insert_this = construct + to_string(messge);
+	strcat( construct, &message );
+	printf("%s\n", construct);
 	
-	char *str = &insert_this[0];
+	char *str = construct;
 	char *query = str;
 	int res = 0;
 
@@ -194,23 +210,28 @@ void send_to_database_error( char message)
 void *send_command( void *not_used)
 {
 	char send[MSG_SIZE];
-	send.sin_family = AF_INET;
-	send.sin_addr.s_addr = INADDR_ANY;
-	send.sin_port = htons(port);
+	char storage[MSG_SIZE];
+	send_this.sin_family = AF_INET;
+	send_this.sin_addr.s_addr = INADDR_ANY;
+	send_this.sin_port = htons(port);
+	
 	while(1)
 	{
-	scanf("%s", buffer);
-	if( buffer != '\0')
+	scanf("%s", storage);
+	printf("\n%s", storage);
+	if( send != '\0')
 		{
 		bzero(send, MSG_SIZE);
-		shot.sin_addr.s_addr = inet_addr(pi_addresss);//change from boardcast address to pi address
-		information_error_check = sendto( sock, send, sizeof(send), 0, (struct sockaddr *) &send, sizeof(send));
+		strcpy(send, storage);
+//		printf("this was stored:%s\n", storage);
+		send_this.sin_addr.s_addr = inet_addr(pi_address);//change from boardcast address to pi address
+		information_error_check = sendto( sock, send, sizeof(send), 0, (struct sockaddr *) &send_this, sizeof(addr));
                if( information_error_check < 0)
               	 {
                 	   printf("Error sending the information to server\n");
          	  }
 		
-		printf("message was sent: %s", send);//comment out once testing is done
+//		printf("message was sent: %s\n", send);//comment out once testing is done
 		//comment this once testing is done and we are ready to test the database side
 	//	send_to_database_log( send );
 		}
@@ -228,13 +249,13 @@ void *receive_update(void *not_used)
   {
         bzero(buffer, MSG_SIZE);
 //	shot.sin_addr.s_addr = inet_addr(historian_address);
-        information_error_check = recvfrom(sock, buffer, MSG_SIZE, 0, (struct sockaddr *)&receive, &receive);
+        information_error_check = recvfrom(sock, buffer, MSG_SIZE, 0, (struct sockaddr *)&receive, &fromlen);
         if( information_error_check < 0)
             {
-                error("that was an error receiving infromation from the sock connection\n");
+                printf("that was an error receiving infromation from the sock connection\n");
                 exit( -1);
             }
-	printf("Received was infomration was: %s", buffer);//comment out once testing is done 
+//	printf("\nReceived was infomration was: %s\n", buffer);//comment out once testing is done 
 
    }
     pthread_exit(0);
